ff10cb2b06351e922c159a1b87f183bafbb3d0cf
Brendan <me@brendanberkley.com>
2015-02-04 23:53:25 -0500

    Home page widget work

--- home.php
+++ home.php
@@ -13,33 +13,10 @@ get_header(); ?>
 		if ( is_active_sidebar( 'signup-section' ) ) : ?> 
 		<div class="signup-section">
 			<?php dynamic_sidebar('Signup Section'); ?>
-		</div> <!-- #banner -->
-		<?php endif; ?>
-		<?php if ( have_posts() ) : ?>
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'content', 'none' ); ?>
-
+		</div> <!-- .signup-section -->
 		<?php endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
