b64e963a98724e44ba5edf8f730cd4795bac47a0
Brendan <me@brendanberkley.com>
2015-02-09 22:24:53 -0500

    Clean up event manager templates a bit

--- page.php
+++ page.php
@@ -12,7 +12,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area col-xs-12 translucent-border">
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
@@ -31,5 +31,7 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+<?php 
+//get_sidebar(); 
+?>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area col-xs-12 translucent-border">
 		<main id="main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
